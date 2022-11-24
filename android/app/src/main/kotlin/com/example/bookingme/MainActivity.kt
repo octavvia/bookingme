9230a6a6fefd97100931bd16d7493cbf786e675d
vviia <auliaoctaviani77@gmail.com>
2022-11-24 14:23:50 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/bookingme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bookingme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Tickets',
+          style: TextStyle(color: Colors.black),
+          textAlign: TextAlign.right,
+        ),
+      ),
+    );
+  }
+}

f87f35fa5454170beb27d7d74d868ff9b1b355fe
vviia <auliaoctaviani77@gmail.com>
2022-11-26 14:54:05 +0700

    add: ticket view

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,6 +1,8 @@
 import 'package:bookingme/utils/app_layout.dart';
 import 'package:bookingme/utils/app_styles.dart';
+import 'package:bookingme/widgets/thick_container.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -14,7 +16,9 @@ class TicketView extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.only(left: 16),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
+            /* showing blue part ticket */
             Container(
               decoration: const BoxDecoration(
                 color: Color(0xFF526799),
@@ -31,10 +35,159 @@ class TicketView extends StatelessWidget {
                       Text('JKT',
                           style: Styles.headLineStyle3
                               .copyWith(color: Colors.white)),
-                      const Spacer(),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const ThickContainer(),
+                      Expanded(
+                        child: Stack(children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context,
+                                  BoxConstraints constraints) {
+                                // print(object)
+                                return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List.generate(
+                                      (constraints.constrainWidth() / 6)
+                                          .floor(),
+                                      (index) => const SizedBox(
+                                        width: 3,
+                                        height: 1,
+                                        child: DecoratedBox(
+                                          decoration: BoxDecoration(
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                    ));
+                              },
+                            ),
+                          ),
+                        ]),
+                      ),
+                      Center(
+                        child: Transform.rotate(
+                          angle: 1.5,
+                          child: const Icon(Icons.local_airport_rounded,
+                              color: Colors.white, size: 24),
+                        ),
+                      ),
+                      const ThickContainer(),
+                      Expanded(
+                        child: Container(),
+                      ),
                       Text('YOG',
                           style: Styles.headLineStyle3
                               .copyWith(color: Colors.white)),
+                    ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: Text('Jakarta',
+                            style: Styles.headLineStyle4
+                                .copyWith(color: Colors.white)),
+                      ),
+                      Text('1H 15M',
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white)),
+                      SizedBox(
+                        width: 100,
+                        child: Text(
+                          'Yogyakarta',
+                          textAlign: TextAlign.end,
+                          style: Styles.headLineStyle4
+                              .copyWith(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+            /* end of blue part ticket 
+          start of orange part ticket*/
+            ,
+            Container(
+              color: Styles.orangeColor,
+              child: Row(
+                children: const [
+                  SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          )),
+                    ),
+                  ),
+                  // Expanded(
+                  //     child: Padding(padding: EdgeInsets.all(12.0)),
+                  //     child: LayoutBuilder(builder:
+                  //         (BuildContext context, BoxConstraints constraints) {
+                  //       return Flex(
+                  //         direction: Axis.horizontal,
+                  //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //         mainAxisSize: MainAxisSize.max,
+                  //         children: List.generate(
+                  //           (constraints.constrainWidth() / 15).floor(),
+                  //           (index) => const SizedBox(
+                  //               width: 5,
+                  //               height: 1,
+                  //               child: DecoratedBox(
+                  //                 decoration:
+                  //                     BoxDecoration(color: Colors.white),
+                  //               )),
+                  //         ),
+                  //       );
+                  //     })),
+                  SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Styles.orangeColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(21),
+                  bottomRight: Radius.circular(21),
+                ),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(children: [
+                        Text('30 Dec 2020',
+                            style: Styles.headLineStyle3
+                                .copyWith(color: Colors.white)),
+                        const Gap(10),
+                        Text('DATE',
+                            style: Styles.headLineStyle4
+                                .copyWith(color: Colors.white)),
+                      ]),
                     ],
                   )
                 ],
--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -6,7 +6,7 @@ class Styles {
   static Color primaryColor = primary;
   static Color textColor = const Color(0xFF3b3b3b);
   static Color bgColor = const Color(0xFFeeedf2);
-  static Color orangeColor = const Color(0xFF526799);
+  static Color orangeColor = const Color(0xFFF37867);
   static TextStyle textStyle = TextStyle(
     color: textColor,
     fontSize: 16,

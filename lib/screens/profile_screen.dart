00eb26936a2ae40b9157b5c79945839f5f526252
vviia <auliaoctaviani77@gmail.com>
2022-12-05 20:02:45 +0700

    add: fix profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -83,7 +83,173 @@ class ProfileScreen extends StatelessWidget {
                 ],
               )
             ],
-          )
+          ),
+          Gap(AppLayout.getHeight(5)),
+          Divider(
+            color: Colors.grey.shade300,
+          ),
+          Gap(AppLayout.getHeight(8)),
+          Stack(
+            children: [
+              Container(
+                height: AppLayout.getHeight(90),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Styles.primaryColor,
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
+                ),
+              ),
+              Positioned(
+                  right: -45,
+                  top: -40,
+                  child: Container(
+                    padding: EdgeInsets.all(
+                      AppLayout.getHeight(30),
+                    ),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.transparent,
+                        border: Border.all(
+                            color: const Color(0xFF264CD2), width: 18)),
+                  )),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: AppLayout.getWidth(20),
+                  vertical: AppLayout.getHeight(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 25,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                          FluentSystemIcons.ic_fluent_lightbulb_filament_filled,
+                          color: Styles.primaryColor,
+                          size: 27),
+                    ),
+                    Gap(AppLayout.getWidth(15)),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('you\'v new reward',
+                            style: Styles.headLineStyle2.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            )),
+                        Text('you have 90 flight in this year',
+                            style: Styles.headLineStyle2.copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white.withOpacity(0.95),
+                              fontSize: 15,
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Gap(AppLayout.getHeight(25)),
+          Text('Acumulated miles', style: Styles.headLineStyle2),
+          Gap(AppLayout.getHeight(20)),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: AppLayout.getWidth(20),
+              vertical: AppLayout.getHeight(20),
+            ),
+            child: Column(
+              children: [
+                Text('1.804.967',
+                    style: TextStyle(
+                      fontSize: 45,
+                      color: Styles.textColor,
+                      fontWeight: FontWeight.w600,
+                    )),
+                Gap(AppLayout.getHeight(40)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('milles accured',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                    Text('12 december 2022',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(20)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('23.042',
+                        style: Styles.headLineStyle4.copyWith(
+                            fontSize: 25, fontWeight: FontWeight.w600)),
+                    Text('Airlines CO',
+                        style: Styles.headLineStyle4.copyWith(
+                            fontSize: 25, fontWeight: FontWeight.w600)),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('milles',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                    Text('recieved from',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(40)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('25.976',
+                        style: Styles.headLineStyle4.copyWith(
+                            fontSize: 25, fontWeight: FontWeight.w600)),
+                    Text('Lion Air',
+                        style: Styles.headLineStyle4.copyWith(
+                            fontSize: 25, fontWeight: FontWeight.w600)),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('milles',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                    Text('recieved from',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(40)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('15.789',
+                        style: Styles.headLineStyle4.copyWith(
+                            fontSize: 25, fontWeight: FontWeight.w600)),
+                    Text('Batik Air',
+                        style: Styles.headLineStyle4.copyWith(
+                            fontSize: 25, fontWeight: FontWeight.w600)),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('milles',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                    Text('recieved from',
+                        style: Styles.headLineStyle4.copyWith(fontSize: 16)),
+                  ],
+                ),
+                Gap(AppLayout.getHeight(40)),
+                Center(
+                  child: Text('View all',
+                      style: Styles.headLineStyle4
+                          .copyWith(fontSize: 16, color: Styles.primaryColor)),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
